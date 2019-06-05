ffddb23e6b2a4f370c5ce5c563a373b93f8ae77d
Medji <17dergachev@gmail.com>
2019-06-05 17:49:18 +0300

    Rendering component of the application settings page

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from './Settings.module.css'
+
+class Settings extends React.Component{
+    render() {
+        return (
+            <div className={styled.settings}>
+                <h2>Settings</h2>
+            </div>
+        )
+    }
+}
+
+export default Settings
