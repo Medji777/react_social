fdb96d467c3706ef83804d195ab118c4ca80111e
Medji <17dergachev@gmail.com>
2019-06-21 22:49:20 +0300

    Implementation for delayed loading of App Component, React Lazy

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+//import App from './components/App';
 import store from './DataBLL/reduxStore';
 //import {Provider} from './StoreContext';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import './index.css';
+import Loading from "./components/common/Loading/Loading";
 
+const App = lazy(()=>import ("./components/App"));
 
 //const reRenderPage = (store) => {
     ReactDOM.render(
         <Provider store={store}>
             {/*Инициализирующий компонент, для создания роутинга в приложении*/}
             <BrowserRouter>
-                <App title="React_Social_Network"/>
+                <Suspense fallback={<Loading wrap={{display: 'flex', alignItems: 'center', minHeight: '100vh'}}/>}>
+                    <App title="React_Social_Network"/>
+                </Suspense>
             </BrowserRouter>
         </Provider>
         ,document.querySelector('#root'));
