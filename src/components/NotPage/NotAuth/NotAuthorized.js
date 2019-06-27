03405ed39860a2348a182a7c10dcd952c995d78b
Medji <17dergachev@gmail.com>
2019-06-27 21:21:40 +0300

    Creating display pages for non-existent paths and unauthorized users

---
+++ src/components/NotPage/NotAuth/NotAuthorized.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import styled from './NotAuth.module.css'
+
+
+let NotAuthorized = () => {
+    return (
+        <div className={styled.notAuth}>
+            <div><p>Вы не авторизированы!</p><p><Link to='/login'>Залогиньтесь</Link> или зарегистрируйтесь</p></div>
+        </div>
+    );
+
+};
+
+
+export default NotAuthorized;
