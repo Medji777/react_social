9d68d54bc086c568a3d341b99ac37d6b70523df1
Medji <17dergachev@gmail.com>
2019-06-29 23:30:13 +0300

    Add validation on the authorized user, Friends Component

--- src/components/Nav/Friends/FriendsContainer.js
+++ src/components/Nav/Friends/FriendsContainer.js
@@ -2,4 +2,4 @@ import React from 'react';
 import Friends from "./Friends";
 import {connect} from "react-redux";
 
-export default connect((state)=>({friends: state.dataFriends.friendsName}),{})(Friends);
+export default connect((state)=>({friends: state.dataFriends.friendsName,isAuth:state.auth.isAuth}),{})(Friends);
