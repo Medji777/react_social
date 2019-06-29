6b757b3028853b2723e4cf223555598e8a8b8ee4
Medji <17dergachev@gmail.com>
2019-06-29 23:19:42 +0300

    Refactoring, as well as creating a popup menu and handling events, SingIn Component

--- src/components/Header/SingIn/SingInContainer.js
+++ src/components/Header/SingIn/SingInContainer.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import {connect} from "react-redux";
 import SingIn from "./SingIn";
-import {logOut} from './../../../DataBLL/authReducer';
+import {logOut,setIsActive} from './../../../DataBLL/authReducer';
 
-const SingInContainer = (props) => {
+class SingInContainer extends React.Component{
 
-    return (
-        <SingIn {...props}/>
-    )
+    addActive = () => {
+        this.props.setIsActive(!this.props.isActive);
+    };
 
-};
+    onWindowClick = () => {
+        this.props.setIsActive(false);
+    };
+
+    componentDidMount() {
+        window.addEventListener("click", this.onWindowClick);
+    }
+
+    componentWillUnmount() {
+        this.props.onWindowClick();
+        window.removeEventListener("click", this.onWindowClick);
+    }
+
+    render() {
+        return (
+            <SingIn {...this.props} addActive={this.addActive}/>
+        )
+    }
+}
 
 const mapStateToProps = (state) => {
-    let {isAuth, userInfo, message} = state.auth;
+    let {isAuth, userInfo, message,isActive} = state.auth;
     return {
         isAuth,
         userInfo,
-        message
+        message,
+        isActive
     }
 };
 
-export default connect(mapStateToProps,{logOut})(SingInContainer);
+export default connect(mapStateToProps, {logOut,setIsActive})(SingInContainer);
