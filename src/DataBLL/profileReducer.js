73b8db06c89e91799296cb4eed193bd972fae1c3
Medji <17dergachev@gmail.com>
2019-07-01 20:16:50 +0300

    Update and create user profile editing functionality, profileReducer

--- src/DataBLL/profileReducer.js
+++ src/DataBLL/profileReducer.js
@@ -1,8 +1,8 @@
 import ConstantType from "./ConstantType";
-import {setUserAvatarUrl} from './authReducer';
+import {setUserAvatarAndName} from './authReducer';
 import API from "../DAL/api";
 
-const {ADD_POST, UPDATE_NEW_POST_TEXT, SET_USER_INFO} = ConstantType;
+const {ADD_POST,UPDATE_NEW_POST_TEXT,SET_USER_INFO,SET_CURRENT_USER_ID,SET_AUTH_USER_INFO,IS_EDIT,IS_LOAD_EDIT,SET_CHANGE_AUTH_PROFILE} = ConstantType;
 const initialState = {
     post: [
         {id: '1', src: 'https://www.abc.net.au/news/image/8314104-1x1-940x940.jpg', text: 'post 1'},
@@ -11,7 +11,11 @@ const initialState = {
         {id: '4', src: 'https://www.abc.net.au/news/image/8314104-1x1-940x940.jpg', text: 'post 4'}
     ],
     newTextPost: '',
-    profileInfo: null
+    profileInfo: null,
+    userAuthProfileInfo: null,
+    currentUserId: null,
+    isEdit: false,
+    isLoadEdit: false
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -43,6 +47,36 @@ const profileReducer = (state = initialState, action) => {
                 profileInfo: action.profileInfo
             }
         }
+        case SET_AUTH_USER_INFO: {
+            return {
+                ...state,
+                userAuthProfileInfo: action.userAuthProfileInfo
+            }
+        }
+        case SET_CURRENT_USER_ID: {
+            return {
+                ...state,
+                currentUserId: action.id
+            }
+        }
+        case IS_EDIT: {
+            return {
+                ...state,
+                isEdit: action.isEdit
+            }
+        }
+        case IS_LOAD_EDIT: {
+            return {
+                ...state,
+                isLoadEdit: action.isLoadEdit
+            }
+        }
+        case SET_CHANGE_AUTH_PROFILE: {
+            return {
+                ...state,
+                userAuthProfileInfo: {...state.userAuthProfileInfo,...action.profile}
+            }
+        }
         default:
             return state;
     }
@@ -50,19 +84,38 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPost = () => ({type: ADD_POST});
 export const updateNewPostText = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
-export const setUserInfo = (profileInfo) => ({type: SET_USER_INFO, profileInfo});
+export const setUserInfo = (profileInfo) => ({type:SET_USER_INFO,profileInfo});
+export const setAuthUserInfo = (userAuthProfileInfo) => ({type:SET_AUTH_USER_INFO,userAuthProfileInfo});
+//export const setCurrentUserId = (id) => ({type: SET_CURRENT_USER_ID,id});
+export const setIsEdit = (isEdit) => ({type: IS_EDIT, isEdit});
+export const setIsLoadEdit = (isLoadEdit) => ({type: IS_LOAD_EDIT,isLoadEdit});
+export const setChangeAuthProfile = (profile) => ({type: SET_CHANGE_AUTH_PROFILE,profile});
 
 export const getUserProfileInfo = (userId, isAuthUser = false) => (dispatch) => {
-    if (userId) {
+    debugger;
+    if(userId){
         API.getProfileInfo(userId)
             .then(res => {
                 if (isAuthUser) {
-                    dispatch(setUserAvatarUrl(res.data.photos.small));
+                    dispatch(setUserAvatarAndName(res.data.photos.small,res.data.fullName));
+                    dispatch(setAuthUserInfo({...res.data}));
                 } else {
                     dispatch(setUserInfo({...res.data}));
                 }
             })
     }
+};
+
+export const setAuthUserProfile = (userAuthProfileInfo) => (dispatch) => {
+    debugger;
+    dispatch(setIsLoadEdit(true));
+    API.setAuthProfileInfo(userAuthProfileInfo)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setAuthUserInfo({...userAuthProfileInfo}));
+                dispatch(setIsLoadEdit(false));
+            }
+        })
 };
 
 export default profileReducer;
