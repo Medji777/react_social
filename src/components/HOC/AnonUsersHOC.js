f0c89d56b5977f0638474e26cedfff401c6b608f
Medji <17dergachev@gmail.com>
2019-07-21 02:48:12 +0300

    Minor changes

--- src/components/HOC/AnonUsersHOC.js
+++ src/components/HOC/AnonUsersHOC.js
@@ -3,17 +3,6 @@ import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import {compose} from "redux";
 
-// export const redirectAnonUsersHoc = (Component) => {
-//     const overComponent = (props) => {
-//
-//         if (!props.isAuth) {
-//             return <Redirect to='/not-auth'/>
-//         }
-//
-//         return <Component {...props}/>
-//     };
-//     return connect((state) => ({isAuth: state.auth.isAuth}))(overComponent);
-// };
 const redirectAnonUsersHoc = (Component) => (props) => {
 
     if (!props.isAuth) {
