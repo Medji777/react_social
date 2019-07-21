0e72831f8f07f4fc53287f7d138b9e4d1454a1b2
Medji <17dergachev@gmail.com>
2019-07-21 23:59:18 +0300

    Minor refactoring

--- src/components/Edit/EditProfileUser.js
+++ src/components/Edit/EditProfileUser.js
@@ -1,52 +1,31 @@
 import React from 'react';
-import {reduxForm,change} from "redux-form";
+import {reduxForm} from "redux-form";
 import {connect} from "react-redux";
 import AnonUsersHOC from "../HOC/AnonUsersHOC";
 import {setAuthUserProfile,setChangeAuthProfile} from './../../DataBLL/profileReducer';
 import EditFormUser from "./EditFormUser";
 import {compose} from "redux";
 
-class EditProfileUser extends React.Component{
+const EditProfileUser = ({setAuthUserProfile, profile, isLoadEdit}) => {
 
-    getInputValue = (value) => {
-        this.props.setAuthUserProfile && this.props.setAuthUserProfile(value);
+    const getInputValue = (value) => {
+        setAuthUserProfile && setAuthUserProfile(value);
     };
 
-    // setChangeValue = (values,dispatch,props,previousValues) => {
-    //     debugger
-    //     if(values !== previousValues){
-    //         dispatch(this.props.change('editForm',`${props.registeredFields}`,values))
-    //         // setChangeAuthProfile && setChangeAuthProfile(values);
-    //     }
-    // };
-    //
-    // componentDidUpdate(prevProps, prevState, snapshot) {
-    //     this.setChangeValue();
-    // }
+    return (
+        <div>
+            <h2>EDIT</h2>
+            <EditReduxForm onSubmit={getInputValue} initialValues={profile} isLoadEdit={isLoadEdit}/>
+        </div>
+    )
 
-    render() {
-        const {profile,isLoadEdit} = this.props;
-        return (
-            <div>
-                <h2>EDIT</h2>
-                <EditReduxForm onSubmit={this.getInputValue} initialValues={profile} isLoadEdit={isLoadEdit}/>
-            </div>
-        )
-    }
-}
+};
 
 const EditReduxForm = reduxForm({
-    form: 'editForm',
-    // onChange(values,dispatch,props,previousValues){
-    //     if(values !== previousValues){
-    //         dispatch(this.props.change('editForm',`${props.registeredFields}`,values))
-    //     }
-    // },
-    // enableReinitialize: true,
-    // touchOnChange: true
+    form: 'editForm'
 })(EditFormUser);
 
 export default compose(connect((state) => ({
     profile: state.dataProfile.userAuthProfileInfo,
     isLoadEdit: state.dataProfile.isLoadEdit,
-}), {setAuthUserProfile,setChangeAuthProfile,change}),AnonUsersHOC)(EditProfileUser);
+}), {setAuthUserProfile,setChangeAuthProfile}),AnonUsersHOC)(EditProfileUser);
