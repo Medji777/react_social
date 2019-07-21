5f0ec7ca8335efb2d4c66530b6b4f3d79c56c720
Medji <17dergachev@gmail.com>
2019-07-21 04:26:56 +0300

    Refactoring and creating instance axios request for updating avatars

--- src/DAL/axios-instance.js
+++ src/DAL/axios-instance.js
@@ -1,9 +1,16 @@
 import * as axiosLib from 'axios';
 
-const axios = axiosLib.create({
+export const axios = axiosLib.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,  // Включает cookie (куки)
     headers: {'API-KEY': 'cf468da1-4098-4d6a-a301-a0693e4da6ed'}
 });
 
-export default axios;
+export const axiosFile = axiosLib.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,  // Включает cookie (куки)
+    headers: {
+        'API-KEY': 'cf468da1-4098-4d6a-a301-a0693e4da6ed',
+        'Content-Type': 'multipart/form-data'
+    }
+});
