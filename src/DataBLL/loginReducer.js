61344c4551af0f2578fba6d4f3e63ba6262c80c2
Medji <17dergachev@gmail.com>
2019-07-26 22:09:55 +0300

    Adding functionality captcha, loginReducer

--- src/DataBLL/loginReducer.js
+++ src/DataBLL/loginReducer.js
@@ -3,7 +3,7 @@ import {setIsAuth,getUserInfoAuth} from "./authReducer";
 import API from "../DAL/api";
 
 
-const {SET_STATUS_LOGIN,SET_MESSAGE_LOGIN,SET_INITIAL_ID} = ConstantType;
+const {SET_STATUS_LOGIN,SET_MESSAGE_LOGIN,SET_INITIAL_ID,GET_CAPTCHA} = ConstantType;
 
 export const statuses = {
     INIT: 'INIT',
@@ -16,7 +16,8 @@ export const statuses = {
 let initialState = {
     status: statuses.INIT,
     message: '',
-    currentId: null
+    currentId: null,
+    captchaUrl: null
 };
 
 const loginReducer = (state = initialState,action) => {
@@ -39,6 +40,12 @@ const loginReducer = (state = initialState,action) => {
                 currentId: action.id
             }
         }
+        case GET_CAPTCHA: {
+            return {
+                ...state,
+                captchaUrl: action.captcha
+            }
+        }
         default: {
             return state
         }
@@ -48,22 +55,32 @@ const loginReducer = (state = initialState,action) => {
 export const setStatus = (status) => ({type: SET_STATUS_LOGIN, status});
 export const setMessage = (message) => ({type: SET_MESSAGE_LOGIN, message});
 export const setUserId = (id) => ({type: SET_INITIAL_ID, id});
+export const getCaptcha = (captcha) => ({type: GET_CAPTCHA, captcha});
 
-export const login = (email, password, rememberMe, c) => (dispatch) => {
+export const login = (email, password, rememberMe, captchaUrl) => (dispatch) => {
     dispatch(setStatus(statuses.INPROGRESS));
-    API.logIn(email, password, rememberMe)
+    API.logIn(email, password, rememberMe,captchaUrl)
         .then(res => {
             if (res.data.resultCode === 0) {
                 dispatch(setStatus(statuses.SUCCESS));
                 dispatch(setUserId(res.data.data.userId));
                 dispatch(setIsAuth(true));
                 dispatch(getUserInfoAuth());
+                dispatch(getCaptcha(null))
             } else {
                 dispatch(setMessage(res.data.messages[0]));
                 dispatch(setStatus(statuses.ERROR));
             }
+            if(res.data.resultCode === 10){
+                dispatch(captcha());
+            }
         })
         .catch(e => console.log(e.message))
+};
+
+export const captcha = () => async (dispatch) => {
+    let res = await API.captcha();
+    dispatch(getCaptcha(res.data.url));
 };
 
 export default loginReducer;
