99a016966f1087b4d5f85ea63b9a07ea5ec40a02
Medji <17dergachev@gmail.com>
2019-09-04 17:43:51 +0300

    Create and add selectors

--- src/DataBLL/selectors.js
+++ src/DataBLL/selectors.js
@@ -1,10 +1,19 @@
 import {createSelector} from 'reselect';
 
-const getUsersName = state => state.name;
-const getUsers = state => [...state.users];
+// users
+export const getUsersFromState = state => state.usersPage.users;
+export const getCurrentPageUsers = state => state.usersPage.currentPage;
+export const getCount = state => state.usersPage.count;
+export const getTotalCount = state => state.usersPage.totalCount;
+export const getIsLoading = state => state.usersPage.isLoading;
+export const getIsLoadingSearch = state => state.usersPage.isLoadingSearch;
+export const getToggleDisable = state => state.usersPage.toggleDisable;
+
+const getUsersName = state => state.usersPage.searchUsers.name;
+const getUsersSearch = state => [...state.usersPage.searchUsers.users];
 
 export const UsersSelector = createSelector(
-    [ getUsersName, getUsers ],
+    [ getUsersName, getUsersSearch ],
     (name, usersList) => {
         name = escapeRegExp(name);
         let regexp = new RegExp(`(^| )${name}`,'ig');
@@ -16,3 +25,34 @@ export const UsersSelector = createSelector(
 const escapeRegExp = (string) => {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 };
+
+// dialogs
+export const getSelectDialogId = state => state.dataDialogs.selectDialogId;
+export const getDataDialogs = state => state.dataDialogs;
+export const getTotalCountMessages = state => state.dataDialogs.totalCountMessages;
+export const getCurrentPageDialogs = state => state.dataDialogs.currentPage;
+export const getMessageDelete = state => state.dataDialogs.messageDelete;
+
+// auth
+export const getIsAuth = state => state.auth.isAuth;
+export const getUserInfo = state => state.auth.userInfo;
+export const getUserId = state => state.auth.userInfo.userId;
+export const getMessage = state => state.auth.message;
+export const getIsActive = state => state.auth.isActive;
+export const getIsInitialized = state => state.auth.isInitialized;
+
+// friends
+export const getFriends = state => state.dataFriends.friendsName;
+
+// profile
+export const getPost = state => state.dataProfile.post;
+export const getCurrentUserId = state => state.dataProfile.currentUserId;
+export const getCountNewMessage = state => state.dataDialogs.countNewMessage;
+export const getGetDialogsCountMode = state => state.dataDialogs.getDialogsCountMode;
+
+// login
+export const getCurrentId = state => state.login.currentId;
+
+// app
+export const getWidthScreen = state => state.app.widthScreen;
+export const getIsWidthResizeMode = state => state.app.isWidthResizeMode;
