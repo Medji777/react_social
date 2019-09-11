9fe8c876683b88c70c89c19ea9221aa2f59b02ef
Medji <17dergachev@gmail.com>
2019-09-11 03:41:11 +0300

    Update Friends Component

--- src/components/Nav/Friends/FriendsContainer.js
+++ src/components/Nav/Friends/FriendsContainer.js
@@ -1,5 +1,6 @@
-import React from 'react';
+//import React from 'react';
 import Friends from "./Friends";
 import {connect} from "react-redux";
+import {getFriends, getIsAuth} from "../../../DataBLL/selectors";
 
-export default connect((state)=>({friends: state.dataFriends.friendsName,isAuth:state.auth.isAuth}),{})(Friends);
+export default connect((state)=>({friends: getFriends(state),isAuth:getIsAuth(state)}),{})(Friends);
