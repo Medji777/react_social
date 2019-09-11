14e9f2eff5e9b544c0ea47622cd2bf7883b6cd25
Medji <17dergachev@gmail.com>
2019-09-11 05:05:03 +0300

    Update and refactoring Posts Components

--- src/components/Content/MyPost/MyPostСontainer.js
+++ src/components/Content/MyPost/MyPostСontainer.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import MyPost from "./MyPost";
-import {addPost, updateNewPostText} from "../../../DataBLL/profileReducer";
+import {addPost} from "../../../DataBLL/profileReducer";
 import {connect} from "react-redux";
+import {reset} from 'redux-form';
+import {getIsAuth, getPost} from "../../../DataBLL/selectors";
 
-const mapStateToProps = (state) => {
-    const {post,newTextPost} = state.dataProfile;
-    return {
-        post,
-        newTextPost
-    }
+const MyPostContainer = ({addPost,updateNewPostText,...props}) => {
+
+    const sendPostText = (value) => {
+          !!value && addPost(value.post);
+    };
+
+    const resetForm = (result,dispatch) => {
+        dispatch(reset('post'))
+    };
+
+    return <MyPost onSubmit={sendPostText} onSubmitSuccess={resetForm} {...props}/>
 };
 
-export default connect(mapStateToProps,{addPost,updateNewPostText})(MyPost);
+export default connect((state) => ({post: getPost(state),isAuth: getIsAuth(state)}),{addPost,reset})(MyPostContainer);
