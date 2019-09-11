c547c75758b4192b824f6fa89d05ed4a0fda5d13
Medji <17dergachev@gmail.com>
2019-09-11 03:37:16 +0300

    Minor update UpdatePhoto components

--- src/components/UpdatePhoto/UpdatePhoto.js
+++ src/components/UpdatePhoto/UpdatePhoto.js
@@ -1,35 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from "react-redux";
 import {setUpdatePhoto} from "../../DataBLL/profileReducer";
 import styled from './UpdatePhoto.module.css'
+import {getUserId} from "../../DataBLL/selectors";
 
-class UpdatePhoto extends Component {
+const UpdatePhoto = ({setUpdatePhoto, userId}) => {
 
-    // state = {
-    //     isActive: false,
-    //     isUp: false
-    // };
-
-    update = (e) => {
+    const update = (e) => {
         const formData = new FormData();
-        formData.append('image',e.currentTarget.files[0]);
-        this.props.setUpdatePhoto(formData,this.props.userId);
+        formData.append('image', e.currentTarget.files[0]);
+        setUpdatePhoto(formData, userId);
     };
 
-    render() {
-        return (
-            <div className={styled.update}>
-                {/*<button onClick={(e) => e.currentTarget.nextSibling.click()}>Выберете фото</button>*/}
-                <div>
-                    <p>Выберете фотографию для профиля. Размер строго 300x300</p>
-                    <div className={styled.update__input}>
-                        <label htmlFor='photo'>Выберете фото</label>
-                        <input type='file' name='image' id='photo' onChange={this.update}/>
-                    </div>
+    return (
+        <div className={styled.update}>
+            <div>
+                <p className={styled.update__title}>Выберете фотографию для профиля. Размер строго 300x300</p>
+                <div className={styled.update__input}>
+                    <label htmlFor='photo'>Выберете фото</label>
+                    <input type='file' name='image' id='photo' onChange={update}/>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
-export default connect((state)=>({userId: state.auth.userInfo.userId}),{setUpdatePhoto})(UpdatePhoto);
+export default connect((state)=>({userId: getUserId(state)}),{setUpdatePhoto})(UpdatePhoto);
