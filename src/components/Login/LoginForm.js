3a3cbc99ae15fdddd64dd3b90cc04564317c6567
Medji <17dergachev@gmail.com>
2019-09-11 03:42:51 +0300

    Update and refactoring Login Components

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,7 +1,7 @@
 import React from "react";
-import InputForm from "../common/InputForm/InputForm";
 import {Field} from "redux-form";
 import styled from './Login.module.css';
+import Form from "../common/Form/Form";
 
 const validateOnSert = (value) => {
     if(!value) return 'Input is REQUIRED';
@@ -36,26 +36,26 @@ const validateOnCaptcha = (value) => {
 
 };
 
-const LoginForm = ({errorMessageBlock,getInputValue,statuses,status,handleSubmit,captchaUrl}) => {
+const LoginForm = ({errorMessageBlock,statuses,status,handleSubmit,captchaUrl}) => {
 
     return (
         <form onSubmit={handleSubmit} className={styled.form__login}>
             <div className={styled.field__form_input}>
                 <label htmlFor='login'>Login</label>
-                <Field component={InputForm} id={'login'} type={'text'} name={'text'} validate={[validateOnSert,validateOnLogin]}/>
+                <Field component={Form} typeForm={'input'} id={'login'} type={'text'} name={'text'} validate={[validateOnSert,validateOnLogin]}/>
             </div>
             <div className={styled.field__form_input}>
                 <label htmlFor='Password'>Password</label>
-                <Field component={InputForm} id={'Password'} type={'password'} name={'password'} validate={[validateOnSert,validateOnPassword]} warn={validateOnNormPassword}/>
+                <Field component={Form} typeForm={'input'} id={'Password'} type={'password'} name={'password'} validate={[validateOnSert,validateOnPassword]} warn={validateOnNormPassword}/>
             </div>
             <div className={styled.field__form_checkbox}>
                 <label htmlFor='RememberMe'>Remember Me</label>
-                <Field component={InputForm} id={'RememberMe'} type={'checkbox'} name={'checkbox'}/>
+                <Field component={Form} typeForm={'input'} id={'RememberMe'} type={'checkbox'} name={'checkbox'}/>
             </div>
             {
                 !!captchaUrl && <div className={styled.field__form_captcha}>
                     <img src={captchaUrl} alt='captcha'/>
-                    <Field component={InputForm} id={'captcha'} type={'text'} name={'captcha'} placeholder={'Введите captcha'} validate={[validateOnSert,validateOnCaptcha]}/>
+                    <Field component={Form} typeForm={'input'} id={'captcha'} type={'text'} name={'captcha'} placeholder={'Введите captcha'} validate={[validateOnSert,validateOnCaptcha]}/>
                 </div>
             }
             <button type='submit' disabled={status === statuses.INPROGRESS}>LogIn</button>
