0c5e01ade5e58da48ef99a792a99dcf31875fd25
Medji <17dergachev@gmail.com>
2019-09-11 04:02:50 +0300

    Update and refactoring Edit Components

--- src/components/Edit/EditFormUser.js
+++ src/components/Edit/EditFormUser.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Field} from "redux-form";
-import styled from './Edit.module.css';
-import InputForm from "../common/InputForm/InputForm";
+import {Field, reduxForm} from "redux-form";
 import Loading from "../common/Loading/Loading";
+import Form from "../common/Form/Form";
+import styled from './Edit.module.css';
 
 const validOnInput = (value) => {
     if (!value) return 'Input is REQUIRED';
@@ -16,56 +16,55 @@ const validOnInputContacts = (value) => {
     return undefined;
 };
 
-
-const EditFormUser = ({initialValues, handleSubmit, isLoadEdit}) => {
-
-    //  console.log(initialValues);
+const EditFormUser = React.memo(({initialValues, handleSubmit, isLoadEdit,isWidthResizeMode}) => {
 
     const wrap = {transform: 'translateY(-20%)', minHeight: 'auto'};
 
-    if (!initialValues) {
-        return <div className={styled.edit__loading}><Loading wrap={wrap}/></div>
-    }
-
+    if (!initialValues) return <div className={styled.edit__loading}><Loading wrap={wrap}/></div>;
+    
     const {contacts} = initialValues;
 
     return <form onSubmit={handleSubmit} className={styled.form__edit}>
         <div className={styled.form__edit_left}>
             <div>
                 <label htmlFor='fullName'>Full name: </label>
-                <Field component={InputForm} id='fullName' type='text' name='fullName'
-                       validate={[validOnInput]}/>
+                <Field component={Form} id='fullName' type='text' name='fullName'
+                       validate={[validOnInput]} typeForm={'input'}/>
             </div>
             <div>
                 <label htmlFor='aboutMe'>About Me</label>
-                <Field component={InputForm} id='aboutMe' type='text' name='aboutMe'/>
+                <Field component={Form} id='aboutMe' type='text' name='aboutMe' typeForm={'textarea'}/>
             </div>
             <div>
                 <label>Contacts: </label>
                 <div className={styled.contacts__wrap}>
                     {contacts && Object.keys(contacts).map((key) =>
-                        <Field component={InputForm} id={`${key}`} key={key} type='text'
+                        <Field component={Form} id={`${key}`} key={key} type='text'
                                name={`contacts.${key}`}
-                               placeholder={`${key}`} validate={[validOnInputContacts]}/>
+                               placeholder={`${key}`} validate={[validOnInputContacts]} typeForm={'input'}/>
                     )}
                 </div>
             </div>
             <div>
                 <label htmlFor={'lookingForAJob'}>looking for a job: </label>
-                <Field component={InputForm} id='lookingForAJob' type='checkbox'
-                       name='lookingForAJob'/>
+                <Field component={Form} id='lookingForAJob' type='checkbox'
+                       name='lookingForAJob' typeForm={'input'}/>
             </div>
             <div>
                 <label htmlFor={'lookingForAJobDescription'}>looking for a job, description: </label>
-                <Field component={InputForm} id='lookingForAJobDescription' type='text'
-                       name='lookingForAJobDescription'/>
+                <Field component={Form} id='lookingForAJobDescription' type='text'
+                       name='lookingForAJobDescription' typeForm={'input'}/>
             </div>
             <button type='submit' disabled={isLoadEdit}>Save</button>
         </div>
-        <div className={styled.form__edit_right}>
+        {!isWidthResizeMode && <div className={styled.form__edit_right}>
             {isLoadEdit && <Loading wrap={wrap}/>}
-        </div>
+        </div>}
     </form>
-};
+},(prevProps,nextProps) => {
+    if(prevProps.initialValues === nextProps.initialValues && prevProps.isLoadEdit === nextProps.isLoadEdit) {
+        return true
+    }
+});
 
-export default EditFormUser;
+export default reduxForm({form: 'editForm'})(EditFormUser);
